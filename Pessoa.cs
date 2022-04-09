6410f267a507346206da0cf8254043e7841ab079
Camily Moreira <camily.camargo2004@gmail.com>
2022-04-09 16:50:43 -0300

    Adicição de novas funções: situação do usuário, clasificação do usuário (está abaixo ou acima do peso) e quanto de quilos o usuário tem que corrigir para ter um peso saudável

--- Pessoa.cs
+++ Pessoa.cs
@@ -3,5 +3,12 @@ public class Pessoa
     public string Nome { get; set; }
     public float Altura { get; set; }
     public float Peso { get; set; }
-    public double IMC { get => Peso / (Altura * Altura); }
+
+    public Pessoa(string nome, float altura, float peso)
+    {
+        Nome = nome;
+        Altura = altura;
+        Peso = peso;
+    }
+    
 }
--- Program.cs
+++ Program.cs
@@ -12,13 +12,43 @@ namespace CalculadoraIMC
             Console.WriteLine("Entre com sua altura em metros. Ex: 1,65");
             float altura = Convert.ToSingle(Console.ReadLine());
 
-            Console.WriteLine("Entre com seu peso em Km. Ex: 67,7");
+            Console.WriteLine("Entre com seu peso em Kg. Ex: 67,7");
             float peso = Convert.ToSingle(Console.ReadLine());
+
+            Pessoa p1 = new Pessoa(nome, altura, peso);
 
             double imc = peso / (altura * altura);
 
             Console.WriteLine("{0} sua altura é {1} e seu peso {2}", nome, altura, peso);
             Console.WriteLine("Seu IMC é {0}", imc);
+            
+            double pcc = (24.9 * peso) / imc;
+            double pc = peso - pcc;
+
+            if (imc < 18.5)
+            {
+                Console.WriteLine("Abaixo do peso!");
+                Console.WriteLine("Você tem que engordar {0}kg para ter um peso saudável", pc);
+            }
+            else if (imc >= 18.5 && imc < 25)
+            {
+                Console.WriteLine("Peso saudável!");
+            }
+            else if (imc >= 25 && imc <= 29.9)
+            {
+                Console.WriteLine("Sobrepeso!");
+                Console.WriteLine("Você tem que imagrecer {0}kg para ter um peso saudável", pc);
+            }
+            else if (imc > 30 && imc < 40)
+            {
+                Console.WriteLine("Obesidade!");
+                Console.WriteLine("Você tem que imagrecer {0}kg para ter um peso saudável", pc);
+            }
+            else
+            {
+                Console.WriteLine("Obesidade grave!");
+                Console.WriteLine("Você tem que imagrecer {0}kg para ter um peso saudável", pc);
+            }
         }
     }
 }
